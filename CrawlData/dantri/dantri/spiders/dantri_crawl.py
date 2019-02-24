7c177ec4512ab650995a6d0dcd918e926101d347
Vu Tien Dat <vudat1710@gmail.com>
2019-02-24 22:50:08 +0700

    update new crawl file

--- CrawlData/dantri/dantri/spiders/dantri_crawl.py
+++ CrawlData/dantri/dantri/spiders/dantri_crawl.py
@@ -1,18 +1,16 @@
-# -*- coding: utf-8 -*-
-# from scrapy import Selector
 from scrapy import Spider
 from scrapy import Request
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor 
-from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
 class DantriCrawlSpider(CrawlSpider):
     name = "dantri_crawl"
     allowed_domains = ['dantri.com.vn']
     start_urls = ['https://dantri.com.vn/nhip-song-tre.htm',]
-    COUNT_MAX = 11000
+    COUNT_MAX = 1000
     count = 0
     rules = (
-        Rule(SgmlLinkExtractor(allow=(), restrict_xpaths=('/html/body/form/div[6]/div[1]/div[1]/div[1]/div[4]/div[1]/a')),
+        Rule(LinkExtractor(allow=(), restrict_xpaths=('//*[@id="html"]/body/div[5]/div[1]/div[1]/div[1]/div[2]/div[1]/a')),
         callback='parse_items', follow=True),
     )
 
@@ -25,18 +23,18 @@ class DantriCrawlSpider(CrawlSpider):
                 yield Request(connect_to_url, callback=self.parse_question)
 
     def parse_question(self, response):
-        title = response.xpath('/html/body/form/div[9]/div[3]/div/div[1]/div[1]/div[1]/h1/text()').extract()
-        span = response.xpath('/html/body/form/div[9]/div[3]/div/div[1]/div[1]/div[1]/h2/text()').extract()
+        title = response.xpath('//*[@id="ctl00_IDContent_ctl00_divContent"]/h1/text()').extract()
+        span = response.xpath('//*[@id="ctl00_IDContent_ctl00_divContent"]/h2/text()').extract()
         contents = response.xpath('//*[@id="divNewsContent"]/p/text()').extract()
-        str2 = ''.join(title).encode('utf-8')
-        str3 = ''.join(span).encode('utf-8')
-        f = open("data.txt","a")
+        str2 = ''.join(title)
+        str3 = ''.join(span)
+        f = open("data2.txt","a")
         f.write(str2.strip())
         f.write('. ')
         f.write(str3.strip())
         f.write(' ')
         for content in contents:
-            str1 = ''.join(content).encode('utf-8')
+            str1 = ''.join(content)
             if (str1 == 'Theo ' or str1 == 'Theo'):
                 str1 = ''
             f.write(str1.strip())
